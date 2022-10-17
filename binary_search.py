690e504597a1f53842f426d82f009b7693e3ce67
JTK01 <99972138+JTK01@users.noreply.github.com>
2022-10-17 20:00:40 +0900

    Add files via upload

---
+++ binary_search.py
@@ -0,0 +1,30 @@
+def binary_jae(array,target,start,end):
+    if start > end:
+        return None
+    mid = (start + end)//2
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary(array,target,start,mid-1)
+    else:
+        return binary(array,target,mid+1,end)
+
+def binary_ban(array,target,start,end):
+    while(start <= end):
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+N, target = map(int,input().split())
+array = list(map(int,input().split()))
+
+result = binary_ban(array,target,0,N-1) #binary_jae & binary_ban both work!!
+if result == None:
+    print("찾는게 없네용~")
+else:
+    print(str(result + 1)+"번쨰에 있네용~")
